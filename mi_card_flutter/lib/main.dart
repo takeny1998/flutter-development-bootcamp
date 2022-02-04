b41aa2b9b6ab7647aa8a3fc677e8b50a4db1ddfe
Heebeom Yang <takeny1998@gmail.com>
2022-02-04 18:01:59 +0900

    Complete Challenge 6-3

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
           backgroundColor: Colors.teal,
           body: SafeArea(
@@ -26,14 +27,59 @@ class MyApp extends StatelessWidget {
                   fontFamily: 'Pacifico',
                 ),
               ),
-              Text(
-                'FLUTTER BOOTCAMP TRAINEE',
-                style: TextStyle(
-                  fontSize: 16.0,
-                  color: Colors.teal[100],
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'Source Sans Pro',
-                )
+              Text('FLUTTER BOOTCAMP TRAINEE',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    letterSpacing: 2.5,
+                    color: Colors.teal.shade100,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Source Sans Pro',
+                  )),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      '+82 010 9427 3520',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 18.0,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                    vertical: 10.0, horizontal: 25.0),
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  children: [
+                    Icon(Icons.email, color: Colors.teal),
+                    SizedBox(width: 10.0),
+                    Text('takeny1998@gmail.com',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 18.0,
+                      ),
+                    )
+                  ],
+                ),
               )
             ],
           ))),
