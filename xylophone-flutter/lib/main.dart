240931d66146b1dc1d3a6d41c5fbb3be673b5653
Heebeom Yang <takeny1998@gmail.com>
2022-02-08 19:04:41 +0900

    Complete Section 8

--- xylophone-flutter/lib/main.dart
+++ xylophone-flutter/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -8,8 +9,38 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            children: [
+              buildKey(1),
+              buildKey(2),
+              buildKey(3),
+              buildKey(4),
+              buildKey(5),
+              buildKey(6),
+              buildKey(7),
+            ],
+          )
         ),
+      ),
+    );
+  }
+
+  void playBell(int number) {
+    final player = AudioCache();
+    player.play('note$number.wav');
+  }
+
+  Expanded buildKey(int number) {
+    List<MaterialColor> colorList = [
+      Colors.red, Colors.orange, Colors.yellow,
+      Colors.green, Colors.teal, Colors.blue, Colors.purple];
+
+    return Expanded(
+      child: TextButton(onPressed: (){playBell(number);},
+        child: Container(
+          color: colorList[number - 1],
+        ),
+        style: TextButton.styleFrom(padding: EdgeInsets.zero),
       ),
     );
   }
